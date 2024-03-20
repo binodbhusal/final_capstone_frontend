ff430a1584b3d5aa0a8920a03db85b16fd9dbb37
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-03-20 20:14:13 +0000

    Update API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
 
-  baseURL: 'https://motorhubbackend-production.up.railway.app',
-  REGISTER_URL: 'https://motorhubbackend-production.up.railway.app/users',
-  LOGIN_URL: 'https://motorhubbackend-production.up.railway.app/users/sign_in',
+  baseURL: 'https://motorhubbackend.up.railway.app',
+  REGISTER_URL: 'https://motorhubbackend.up.railway.app/users',
+  LOGIN_URL: 'https://motorhubbackend.up.railway.app/users/sign_in',
 });
